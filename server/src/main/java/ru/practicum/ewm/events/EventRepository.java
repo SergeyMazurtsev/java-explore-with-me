53222f36cdd4269fcfc404da5314b0d32729c8ee
SergeyMazurtsev <sergey.mazurtsev@yandex.ru>
2022-10-27 20:19:23 +0300

    fix copy of code and some validation

--- server/src/main/java/ru/practicum/ewm/events/EventRepository.java
+++ server/src/main/java/ru/practicum/ewm/events/EventRepository.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.events;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.ewm.admin.model.Category;
@@ -11,12 +10,15 @@ import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAllByInitiator(User user, Pageable pageable);
+
     List<Event> findAllByInitiator(User user);
 
     List<Event> findAllByStateAndAnnotationContainingIgnoreCaseOrDescriptionContainingIgnoreCaseOrTitleContainingIgnoreCaseAndAndPaid(
             EventState state, String annotation, String description, String title, Boolean paid);
+
     List<Event> findAllByStateAndAnnotationContainingIgnoreCaseOrDescriptionContainingIgnoreCaseOrTitleContainingIgnoreCase(
             EventState state, String annotation, String description, String title);
+
     List<Event> findAllByPaidAndState(Boolean paid, EventState state);
 
     List<Event> findAllByState(EventState published);
--- server/src/test/java/ru/practicum/ewm/events/EventServiceImplTest.java
+++ server/src/test/java/ru/practicum/ewm/events/EventServiceImplTest.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.events;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
